76884834bd662eceab17470529aa49f413fdd3e4
Jerry <xiarunhao123@163.com>
2023-06-01 16:02:50 +0800

    x

---
+++ src/pages/report/index.vue
@@ -0,0 +1,40 @@
+<script setup name="report">
+
+import { ref } from 'vue';
+import { onMounted } from '@/hooks/onMounted';
+import { useState } from '@/hooks/useState';
+
+// 加载
+const loading = ref(true);
+
+onMounted(() => {
+
+    loading.value = false;
+
+    uni.hideLoading();
+
+});
+
+</script>
+
+<template>
+    <view v-show="!loading" class="content">
+
+
+
+    </view>
+</template>
+  
+<style>
+page {
+    background-color: #ffffff;
+}
+</style>
+
+<style lang="scss" scoped>
+.content {
+    height: 100%;
+    background-color: #fff;
+    margin-top: 20rpx;
+}
+</style>
