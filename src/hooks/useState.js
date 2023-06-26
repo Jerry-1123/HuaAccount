8a47d59ef11c4ae043ac3a9ca2c2c3c394b0b54a
Jerry <xiarunhao123@163.com>
2023-06-26 13:49:22 +0800

    x

--- src/hooks/useState.js
+++ src/hooks/useState.js
@@ -7,58 +7,11 @@ export const useState = () => {
     const userStore = useUserStore();
     const appStore = useAppStore();
 
-    // state
-    const {
-        openId,
-        userId,
-        nickName,
-        avatar,
-        registerTime
-    } = storeToRefs(userStore);
-
-    // actions
-    const {
-        setUserInfo,
-        changeUserInfo
-    } = userStore;
-
-    // state
-    const {
-        tags
-    } = storeToRefs(appStore);
-
-    // getters
-    const {
-        tagsList,
-        tagsGroup,
-        expenseTags,
-        incomeTags,
-    } = storeToRefs(appStore);
-
-    // actions
-    const {
-        setTags
-    } = appStore;
-
     return {
-        // state
-        openId,
-        userId,
-        nickName,
-        avatar,
-        registerTime,
-        tags,
-
-        // getters
-        tagsList,
-        tagsGroup,
-        expenseTags,
-        incomeTags,
-        
-        // actions
-        setUserInfo,
-        changeUserInfo,
-        setTags
+        ...userStore,
+        ...storeToRefs(userStore),
+        ...appStore,
+        ...storeToRefs(appStore)
     };
 
 };
