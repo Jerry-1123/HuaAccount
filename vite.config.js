bdd49920a89956a6fcf574bc0a42b9aa7029ef95
Jerry <xiarunhao123@163.com>
2023-07-06 09:20:51 +0800

    x

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,12 @@ import uni from '@dcloudio/vite-plugin-uni';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    uni()
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
+    plugins: [
+        uni()
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    },
 });
