86100b82d4f17b3c49edc9c5798b900d790b41bd
Jerry <xiarunhao123@163.com>
2024-01-03 09:57:25 +0800

    x

--- src/hooks/onShareAppMessage.js
+++ src/hooks/onShareAppMessage.js
@@ -4,7 +4,7 @@ export const onShareAppMessage = () => onShareApp(() => {
 
     return {
         title: '欢迎来到花花记账本~',
-        path: '/pages/index/index',
+        path: '/pages/home/index',
         imageUrl: 'https://mp-9f46008e-93c5-4337-83cd-181660899272.cdn.bspapp.com/cloudstorage/8c7301f1-aad4-4140-bd8b-3f0a89a67755.jpg'
     };
 
